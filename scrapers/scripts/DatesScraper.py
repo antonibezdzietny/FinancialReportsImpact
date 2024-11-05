d39b2d03d9648324d70756137c6f6b65437dddbb
Antoni Bezdzietny <249327@student.pwr.edu.pl>
2024-11-05 14:46:06 +0100

    Update dates scraper

--- scrapers/scripts/DatesScraper.py
+++ scrapers/scripts/DatesScraper.py
@@ -5,8 +5,10 @@ import re
 
 
 class DatesScraper:
-    @staticmethod
-    def __cast_espi_title(title):
+    def __init__(self):
+        self.notoriaIDs = pd.read_csv("../database/designData/notoriaIDs.csv")
+
+    def __cast_espi_title(self, title):
         year = re.findall("\\d{4}", title)
 
         if "SA-R" in title or "SA-RS" in title:
@@ -20,28 +22,43 @@ class DatesScraper:
         elif "QSr4" in title or "QS 4" in title or "Q4" in title or "Q 4" in title:
             return f"{year[0]}/Q4"
         
-    @staticmethod
-    def __check_request_status_code(response):
+    def __check_request_status_code(self, response):
         try:
             response.raise_for_status()
         except requests.exceptions.HTTPError as e:
             return "Error: " + str(e)
         
-    @staticmethod
-    def get_date_map(ticker):
-        # GET company id
+    def __get_id_locally(self, ticker):
+        try:
+            notoriaId = self.notoriaIDs[self.notoriaIDs["Ticker"] == ticker]["NotoriaID"].item()
+        except:
+            notoriaId = None
+        return notoriaId
+    
+    def __get_id_remote(self, ticker):
         response = requests.get(f"https://www.notoria.pl/api/nol/search/news-issuer?phrase={ticker}") 
-        DatesScraper.__check_request_status_code(response)
+        self.__check_request_status_code(response)
         notoriaId = response.json()[0]["id"]
+        return notoriaId
+
+    def __get_id(self, ticker):
+        # Try to get locally
+        notoriaId = self.__get_id_locally(ticker)
+        if notoriaId == None:
+            notoriaId = self.__get_id_remote(ticker)
+        return notoriaId
+
+    def get_date_map(self, ticker):
+        notoriaId = self.__get_id(ticker)
 
         # GET financial ESPI data 
         response = requests.get(f"https://www.notoria.pl/api/nol/news/espi-ebi?offset=0&limit=1000&notoriaId={notoriaId}&source=espi&type=5&category=6")
-        DatesScraper.__check_request_status_code(response)
+        self.__check_request_status_code(response)
 
         # Data customization
         espi_reports = pd.DataFrame(response.json()["result"][::-1])[["title", "published"]]
         espi_reports["published"] = espi_reports["published"].apply(lambda x : np.datetime64(x[:10]))
-        espi_reports["title"] = espi_reports["title"].apply(lambda x : DatesScraper.__cast_espi_title(x))
+        espi_reports["title"] = espi_reports["title"].apply(lambda x : self.__cast_espi_title(x))
         espi_reports = espi_reports.dropna()
         espi_reports = espi_reports.drop_duplicates(subset="title")
         espi_reports.columns = ["Q_DATE", "D_DATE"]
